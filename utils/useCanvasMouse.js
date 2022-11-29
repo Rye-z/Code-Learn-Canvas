33b83d99efd8a80678c914ce8ccb88b9a02be01c
isearch-zsy <894544438@qq.com>
2022-11-29 18:00:52 +0800

    feat: add useCanvasMouse.js

---
+++ utils/useCanvasMouse.js
@@ -0,0 +1,39 @@
+//将tools定义为window对象的属性，该属性的值是一个对象
+window.tools = {};
+//获取鼠标位置
+
+const addEvent = (element, eventName, callback) => {
+  element.addEventListener(eventName, callback)
+}
+
+window.tools.getMouse = function (element) {
+  //定义一个mouse的对象
+  var mouse = { x: 0, y: 0 };
+  //为传入的元素添加mousemove事件
+  addEvent(element, "mousemove", function (e) {
+    var x, y;
+    //在IE中，event对象是作为window对象的一个属性存在
+    var e = e || window.event;
+    //获取鼠标当前位置，并作兼容处理
+    //兼容Firefox、chrome、IE9及以上
+    if (e.pageX || e.pageY) {
+      x = e.pageX;
+      y = e.pageY;
+    }
+    //兼容IE8及以下，以及混杂模式下的Chrome和Safari
+    else {
+      x  =  e.clientX  +  document.body.scrollLeft  ||  document.
+        documentElement.scrollLeft;
+      y  =  e.clientY  +  document.body.scrollTop  ||  document.
+        documentElement.scrollTop;
+    }
+    //将当前的坐标值减去canvas元素的偏移位置，则x、y为鼠标在canvas中的相对坐标
+    x -= element.offsetLeft;
+    y -= element.offsetTop;
+
+    mouse.x = x;
+    mouse.y = y;
+  })
+  //返回值为mouse对象
+  return mouse;
+}
